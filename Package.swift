838283f9aad9e3c35bb2c5c076d2b88c38a6d64b
davdroman <d@vidroman.dev>
2021-09-28 18:43:25 +0100

    Unhide dev dependencies

--- Package.swift
+++ Package.swift
@@ -15,11 +15,11 @@ let package = Package(
         .library(name: "TextBuilder", targets: ["TextBuilder"]),
     ],
     dependencies: [
-//        .package(name: "SnapshotTesting", url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.8.2"), // dev
-//        .package(name: "Rocket", url: "https://github.com/shibapm/Rocket", from: "0.1.0"), // dev
+        .package(name: "SnapshotTesting", url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.8.2"), // dev
+        .package(name: "Rocket", url: "https://github.com/shibapm/Rocket", from: "0.1.0"), // dev
     ],
     targets: [
         .target(name: "TextBuilder"),
-//        .testTarget(name: "TextBuilderTests", dependencies: ["TextBuilder", "SnapshotTesting"], resources: [ .copy("__Snapshots__") ]), // dev
+        .testTarget(name: "TextBuilderTests", dependencies: ["TextBuilder", "SnapshotTesting"], resources: [ .copy("__Snapshots__") ]), // dev
     ]
 )
