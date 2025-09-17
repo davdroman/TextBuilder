bfb547e4b284bc9861e436424bc711ec122b4dc1
David Roman <2538074+davdroman@users.noreply.github.com>
2025-09-17 23:02:02 +0100

    WIP

--- Sources/TextBuilderMacro/TextBuilderMacro.swift
+++ Sources/TextBuilderMacro/TextBuilderMacro.swift
@@ -1,8 +1,8 @@
-public import SwiftSyntax
-public import SwiftSyntaxMacros
+import SwiftSyntax
+import SwiftSyntaxMacros
 
-public struct TextBuilderMacro: BodyMacro {
-	public static func expansion(
+struct TextBuilderMacro: BodyMacro {
+	static func expansion(
 		of node: AttributeSyntax,
 		providingBodyFor declaration: some DeclSyntaxProtocol & WithOptionalCodeBlockSyntax,
 		in context: some MacroExpansionContext
